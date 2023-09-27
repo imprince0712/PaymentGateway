c82d0986d7d42ade4687d359032ca37e53aad623
Prince <imprince0712@gmail.com>
2023-09-28 00:15:57 +0530

    2nd commit

--- src/main/java/com/paypalpayment/PaypalpaymentApplication.java
+++ src/main/java/com/paypalpayment/PaypalpaymentApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PaypalpaymentApplication {
-
+	// main class
 	public static void main(String[] args) {
 		SpringApplication.run(PaypalpaymentApplication.class, args);
 	}
