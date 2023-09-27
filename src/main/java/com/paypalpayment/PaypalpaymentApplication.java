3804006d83abbd6d9b21783815d7b1d9938cf453
Prince <imprince0712@gmail.com>
2023-09-28 00:10:49 +0530

    1st commit

---
+++ src/main/java/com/paypalpayment/PaypalpaymentApplication.java
@@ -0,0 +1,14 @@
+package com.paypalpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypalpaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypalpaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paypalpayment/PaypalpaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paypalpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalpaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
